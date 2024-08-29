a9a8a4857d6a2bcb5fa2edb4c66b331c1d50d97e
kkmdev94 <foxname91@gmail.com>
2024-08-29 14:21:19 +0900

    add chapter05

---
+++ src/chapter_05/comment.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Comment(props){
+    return (
+        <div>
+            <h1>welecome to the first comment</h1>
+        </div>
+    );
+}
+
+export default Comment;
