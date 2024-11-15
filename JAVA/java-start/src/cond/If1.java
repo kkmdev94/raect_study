f452471834dd6c5447348f939b6b7f42d890fadf
foxname91 <foxname91@gmail.com>
2024-11-15 14:24:44 +0900

    study Java

---
+++ JAVA/java-start/src/cond/If1.java
@@ -0,0 +1,14 @@
+package cond;
+
+public class If1 {
+
+    public static void main(String[] args) {
+        int age = 17;
+
+        if (age >= 18) {
+            System.out.println("man");
+        } else {
+            System.out.println("child");
+        }
+    }
+}
