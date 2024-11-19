b7e251785f9eff8eacc49028ff9f9dd89af95919
kkmdev94 <foxname91@gmail.com>
2024-11-20 00:29:13 +0900

    add study loop

---
+++ JAVA/java-start/src/loop/whilel_1.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class whilel_1 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        count = count + 1;
+        System.out.println("1 = " + count);
+        count = count + 1;
+        System.out.println("2 = " + count);
+        count = count + 1;
+        System.out.println("3 = " + count);
+    }
+}
