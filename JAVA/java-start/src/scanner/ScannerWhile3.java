6f3037aed7f25096c9b71c0b3d65cb2f02cf1574
foxname91 <foxname91@gmail.com>
2024-11-26 16:12:11 +0900

    end study Scanner

--- JAVA/java-start/src/scanner/ScannerWhile3.java
+++ JAVA/java-start/src/scanner/ScannerWhile3.java
@@ -6,5 +6,19 @@ public class ScannerWhile3 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        while (true) {
+            System.out.print("숫자를 입력해 주세요(0이 입력되면 프로그램은 종료됩니다.) : ");
+            int num = sc.nextInt();
+            if (num == 0 ){
+                System.out.println("0이 입력되어 프로그램을 종료합니다.");
+                break;
+            }
+//            sum = sum + num;
+            sum += num;
+        }
+        System.out.println("입력값의 총 합은 : " + sum);
     }
 }
