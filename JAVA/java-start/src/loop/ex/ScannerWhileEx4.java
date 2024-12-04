9650a29da5a683858ebae8b9dbdbb64eb6f33896
kkmdev94 <foxname91@gmail.com>
2024-12-05 00:02:23 +0900

    end Scanner && start Array

--- JAVA/java-start/src/loop/ex/ScannerWhileEx4.java
+++ JAVA/java-start/src/loop/ex/ScannerWhileEx4.java
@@ -6,35 +6,36 @@ public class ScannerWhileEx4 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name;
-        int price;
-        int qunitity;
-        int option = 0;
+        int total = 0;
 
         while (true) {
             System.out.println("1 : 상품 입력, 2 : 결제, 3: 프로그램 종료");
-            option = sc.nextInt();
+            int option = sc.nextInt();
             
             if(option == 1) {
                 System.out.print("상품명을 입력하세요 : " );
-                name = sc.nextLine();
+                String name = sc.nextLine();
                 sc.nextLine();
                 
                 System.out.print("상품의 가격을 입력하세요 : ");
-                price=sc.nextInt();
+                int price=sc.nextInt();
                 
                 System.out.print("구매 수량을 입력하세요 : ");
-                qunitity = sc.nextInt();
+                int qunitity = sc.nextInt();
                 
                 int sum = price * qunitity;
                 
                 System.out.println("상품명 : " + name + " 가격 : " + price + " 수량 : " + qunitity + " 합계 : " + sum);
+
+                total += sum;
             } else if (option == 2) {
-                System.out.println("222");
+                System.out.println("총 비용 : " + total);
+                total = 0;
             }else if (option == 3) {
+                System.out.println("프로그램을 종료합니다.");
                 break;
             }else{
-                System.out.println("number");
+                System.out.println("올바른 숫자를 입력해 주세요");
             }
         }
     }
