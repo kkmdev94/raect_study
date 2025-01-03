c9f0d62db9e7fde81e249bc4a3538a6364399efb
kkmdev94 <foxname91@gmail.com>
2025-01-03 23:06:23 +0900

    end ex

--- JAVA/java-beginner/src/pay/ex0/PayMain1.java
+++ JAVA/java-beginner/src/pay/ex0/PayMain1.java
@@ -11,15 +11,13 @@ public class PayMain1 {
             while (true) {
                 System.out.print("결제 수단을 입력하세요 : ");
                 String payOption = sc.nextLine();
-                if (payOption != "exit") {
-                    System.out.print("결제 금액을 입력하세요 : ");
-                    int amount1 = sc.nextInt();
-                    payService.processPay(payOption, amount1);
-                } else if (payOption == "exit") {
+                if (payOption.equals("exit")) {
                     System.out.println("프로그램을 종료합니다.");
                     break;
                 } else {
-                    System.out.println("잘못 입력하셨습니다. 다시 입력해 주세요");
+                    System.out.print("결제 금액을 입력하세요 : ");
+                    int amount1 = sc.nextInt();
+                    payService.processPay(payOption, amount1);
                 }
                 sc.nextLine();
             }
