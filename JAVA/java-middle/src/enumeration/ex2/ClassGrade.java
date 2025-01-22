9a6145f600a50a90f519354dadbba00a77563ce0
foxname91 <foxname91@gmail.com>
2025-01-22 19:22:42 +0900

    study enum

---
+++ JAVA/java-middle/src/enumeration/ex2/ClassGrade.java
@@ -0,0 +1,11 @@
+package enumeration.ex2;
+
+
+// 회원 등급을 다루는 '클래스'를 만든다. = ClassGrade가 회원 등급을 다루는 클래스
+public class ClassGrade {
+
+
+    public static final ClassGrade BASIC = new ClassGrade();
+    public static final ClassGrade GOLD = new ClassGrade();
+    public static final ClassGrade DIAMOND = new ClassGrade();
+}
