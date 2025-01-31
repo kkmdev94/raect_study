b08ddc09fb5fd869f487aee272cdf1fa02053af0
foxname91 <foxname91@gmail.com>
2025-01-31 18:01:50 +0900

    ex2 ing

--- JAVA/java-middle/src/time/test/TestCalendarPrinter.java
+++ JAVA/java-middle/src/time/test/TestCalendarPrinter.java
@@ -1,8 +1,23 @@
 package time.test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Scanner;
+
 public class TestCalendarPrinter {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("년도를 입력하세요 : ");
+        int year = sc.nextInt();
+        System.out.print("월을 입력하세요 : ");
+        int month = sc.nextInt();
+
+        LocalDate cal = LocalDate.of(year, month, 1);
+        LocalDate lastDayOfWeek = cal.with(TemporalAdjusters.lastDayOfMonth());
+//        System.out.println("lastDayOfWeek = " + lastDayOfWeek);
+
 
     }
 }
