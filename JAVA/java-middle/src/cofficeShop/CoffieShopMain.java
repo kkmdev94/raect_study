d1399100bd7f3e6be205700860ba5006810bfc91
kkmdev94 <foxname91@gmail.com>
2025-02-03 23:12:09 +0900

    coffie

--- JAVA/java-middle/src/cofficeShop/CoffieShopMain.java
+++ JAVA/java-middle/src/cofficeShop/CoffieShopMain.java
@@ -4,5 +4,7 @@ public class CoffieShopMain {
 
     public static void main(String[] args) {
 
+        Manager manager = new Manager();
+        manager.coffie();
     }
 }
--- JAVA/java-middle/src/cofficeShop/Manager.java
+++ JAVA/java-middle/src/cofficeShop/Manager.java
@@ -6,6 +6,9 @@ public class Manager {
         String key = coffie(coffie);
         return key;
     }
+
+    public void coffie() {
+    }
     // 어떤 커피던 받아올 수 있다 새로운 커피가 추가 되도 매니저는 바뀌지 않는다
     // 매장에서 매니저를 이용해 커피를 제공한다.
 }
