d7378c4baa0732015c3d184789f07b3b59661697
kkmdev94 <foxname91@gmail.com>
2025-02-11 23:42:32 +0900

    exception study

--- JAVA/java-middle/src/exception/basic/checked/Client.java
+++ JAVA/java-middle/src/exception/basic/checked/Client.java
@@ -1,8 +1,8 @@
 package exception.basic.checked;
 
 public class Client {
-    public void call() throws MyCheckedException{
+    public void call() throws MyCheckedException{ // throws는 이 폭탄을 내가 처리할 수 없다 라고 하고 밖으로 던지는것
         // 문제 상황
-        throw new MyCheckedException("ex");
+        throw new MyCheckedException("ex"); // throw 실제 폭탄을 터트리는것, 즉 예외를 터트림
     }
 }
