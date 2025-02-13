a426567fdbb0511898a0717169dbcc8e570ded07
foxname91 <foxname91@gmail.com>
2025-02-13 14:08:18 +0900

    study try-catch-finally

--- JAVA/java-middle/src/exception/ex2/MainV2.java
+++ JAVA/java-middle/src/exception/ex2/MainV2.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class MainV2 {
 
     public static void main(String[] args) throws NetworkClientExceptionV2 {
-//        NetworkServiceV1_1 service = new NetworkServiceV1_1();
-//        NetworkServiceV1_2 service = new NetworkServiceV1_2();
-        NetworkServiceV2_1 service = new NetworkServiceV2_1();
+//        NetworkServiceV2_1 service = new NetworkServiceV2_1();
+//        NetworkServiceV2_2 service = new NetworkServiceV2_2();
+//        NetworkServiceV2_3 service = new NetworkServiceV2_3();
+//        NetworkServiceV2_4 service = new NetworkServiceV2_4();
+        NetworkServiceV2_5 service = new NetworkServiceV2_5();
 
         Scanner sc = new Scanner(System.in);
         while (true) {
