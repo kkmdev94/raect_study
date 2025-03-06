5a68b20bc4cd6cd6a658e4ef57f5888380a56402
kkmdev94 <foxname91@gmail.com>
2025-03-06 23:06:20 +0900

    fix HashSetV1Main

--- JAVA/java-middle2/src/collection/set/MyHashSetV1.java
+++ JAVA/java-middle2/src/collection/set/MyHashSetV1.java
@@ -49,15 +49,34 @@ public class MyHashSetV1 {
     public boolean remove(int value) {
         int hashIndex = hashIndex(value);
         LinkedList<Integer> bucket = buckets[hashIndex];
-        bucket.remove(Integer.valueOf(value));
+        boolean result = bucket.remove(Integer.valueOf(value));
         /*
         리무브 조심해야될 점 :
             - 리무브는 두가지 종류가 있다. 하나는 인덱스를 지우는 법, 하나는 래퍼 타입으로 오브젝트형으로 넘겨서 링크드 리스트 안에 있는 값을 지우는 방법 두가지가 있다.
             ctrl + P로 인트형인지 오브젝트 형인지 확인이 가능하며, 인트형을 매개변수로 넘겨주면 인덱스 자체가 삭제되는거고 래퍼 타입으로 넘겨주어야만 인덱스 값 안의 내용을 찾아서 지운다.
          */
+        if (result) {
+            size--;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     private int hashIndex(int value) {
         return value % capacity;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV1{" +
+                "buckets=" + Arrays.toString(buckets) +
+                ", size=" + size +
+                ", capacity=" + capacity +
+                '}';
     }
 }
