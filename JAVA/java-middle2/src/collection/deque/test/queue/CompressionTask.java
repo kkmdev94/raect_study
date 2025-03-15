5413e272500f68891c2b7876911e1c73966c5f53
kkmdev94 <foxname91@gmail.com>
2025-03-15 23:22:42 +0900

    Queue ex

---
+++ JAVA/java-middle2/src/collection/deque/test/queue/CompressionTask.java
@@ -0,0 +1,8 @@
+package collection.deque.test.queue;
+
+public class CompressionTask implements Task{
+    @Override
+    public void exectue() {
+        System.out.println("데이터 압축...");
+    }
+}
---
+++ JAVA/java-middle2/src/collection/deque/test/queue/TaskScheduler.java
@@ -0,0 +1,11 @@
+package collection.deque.test.queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class TaskScheduler {
+
+    private Queue<Task> tasks = new ArrayDeque<>();
+
+
+}
