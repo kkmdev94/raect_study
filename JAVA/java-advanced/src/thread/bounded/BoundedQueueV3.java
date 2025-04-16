4309301230de6dc509e9cad866f8451d16f2811c
kkmdev94 <foxname91@gmail.com>
2025-04-16 22:29:54 +0900

    Condition

--- JAVA/java-advanced/src/thread/bounded/BoundedQueueV3.java
+++ JAVA/java-advanced/src/thread/bounded/BoundedQueueV3.java
@@ -7,9 +7,10 @@ import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
 
 public class BoundedQueueV3 implements BoundedQueue{
-    /* synchronized로 인해 동기화가 이루어지고 스레드는 하나만 락을 가지고 들어올 수 있기 때문에.
-    생산자가 먼져 실행되면 데이터를 넣지도 버리지도 못하고 공간이 나올때까지 무한대기가 걸리고 소비자는 생산자가 락을 가지고 있기에 진입하지 못하고 블록이 된다.
-    소비자가 먼져 실행되면 데이터를 가져와야 되는데 데이터가 없으니 가져오지 못하고, 1번 소비자부터 락을 가지고 빠져나오지 못하기에 블록이 되고 무한 대기를 이룬다.
+    /* wait(), notify(), notifyAll() 은 Object의 메서드로서, Object는 모든 것의 부모이기에 모든것을 담을 수 있다.
+    따라서, wait()는 스레드 대기 집합을 통해 락을 반납하고 대기 할 수 있으며 notify()는 스레드 대기 집합에 있는 것을 깨울 수 있다.
+    이로 인해 Thread를 제어 할 수 있는 자료 구조를 완성했지만, 이 방식의 단점은 '스레드 대기 집합이 하나'라는 것이다
+    스레드 대기 집합이 하나이기에 원하는것을 깨울 수 없으며(소비자가 생산자를 / 생산자가 소비자를) 이로인해 약간의 비효율이 발생한다(로직이 작동하는데에는 문제가 없다)
     */
 
     private final Queue<String> queue = new ArrayDeque<>();
