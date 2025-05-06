7eaf417e81971af9d91f044fbd571ad61af9c3b8
foxname91@gmail.com <foxname91@gmail.com>
2025-05-06 21:17:10 +0900

    JdbcTemplate

---
+++ Spring/src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,47 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional // Auto commit이 아니라면 트랜젝션에서 커밋전까지 대기한다. 하지만 테스트에서 트랜젝션을 걸면 다음 테스트를 바로 진행 할 수 있게 테스트 완료 후 바로 롤백한다.
+/*
+순수하게 자바 코드로 하면서 최소한의 단위로 하는 것을 단위테스트라 하고
+스프링과 db까지 연동해서 하는것을 통합테스트라고 표현하는데, 가급적이면 순수한 단위 테스트가 훨씬 좋은 테스트일 확률이 높다
+ */
+class MemberServiceIntegrationTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //Given
+        Member member = new Member();
+        member.setName("hello");
+        //When
+        Long saveId = memberService.join(member);
+        //Then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
+    }
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //Given
+        Member member1 = new Member();
+        member1.setName("spring");
+        Member member2 = new Member();
+        member2.setName("spring");
+        //When
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+}
--- Spring/src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ Spring/src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -1,17 +1,28 @@
 package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
-import org.assertj.core.api.Assertions;
+import hello.hello_spring.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService service = new MemberService();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
 
     @Test
     public void save() {
@@ -19,9 +30,9 @@ class MemberServiceTest {
         member.setId(1L);
         member.setName("spring");
 
-        service.join(member);
+        memberService.join(member);
 
-        List<Member> members = service.findMembers();
+        List<Member> members = memberService.findMembers();
         assertThat(members);
     }
 }
