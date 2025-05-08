f0ea9fda766ced37c1e9d717008c7332cbd846e5
foxname91@gmail.com <foxname91@gmail.com>
2025-05-08 22:02:20 +0900

    refactoring file

--- Spring2/src/main/java/hello/core/CoreAapplication.java
+++ Spring2/src/main/java/hello/core/CoreAapplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package hello.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CoreAapplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CoreAapplication.class, args);
 	}
 
 }
