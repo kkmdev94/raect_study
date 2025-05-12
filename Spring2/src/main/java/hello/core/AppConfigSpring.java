50632af19c3305ad8ec5ce49c7f0167929751d54
foxname91 <foxname91@gmail.com>
2025-05-12 17:57:43 +0900

    Spring refatoring

--- Spring2/src/main/java/hello/core/AppConfigSpring.java
+++ Spring2/src/main/java/hello/core/AppConfigSpring.java
@@ -1,29 +1,35 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class AppConfigSpring { // 스프링 리팩토링
 
-public class AppConfig { // 실제 동작에 필요한 구현 객체를 여기서 다 생성하고 컨트롤한다.
-
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
