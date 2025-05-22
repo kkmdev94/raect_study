461898f188bbd8abfc8ac85c67969a50021ff14b
foxname91 <foxname91@gmail.com>
2025-05-22 18:03:38 +0900

    Provider

--- Spring2/src/test/java/hello/core/scope/SingletonWithPrototypeTest3.java
+++ Spring2/src/test/java/hello/core/scope/SingletonWithPrototypeTest3.java
@@ -3,18 +3,18 @@ package hello.core.scope;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SingletonWithPrototypeTest2 {
+public class SingletonWithPrototypeTest3 {
 
     @Test
     void prototypeFind() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-
         PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
         prototypeBean1.addCount();
         assertThat(prototypeBean1.getCount()).isEqualTo(1);
@@ -27,26 +27,24 @@ public class SingletonWithPrototypeTest2 {
     @Test
     void singletonClientUsePrototype() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
-
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count1 = clientBean1.logic();
         assertThat(count1).isEqualTo(1);
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입되어 계속 해당 빈을 사용.
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider; // getObject를 호출하면 getObject가 스프링 컨테이너에서 프로토타입 빈을 찾아서 반환해준다.
+        // 즉, 우리가 애플리케이션 컨텍스트한테 직접 찾는게 아니라 찾아주는 기능만 제공
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
