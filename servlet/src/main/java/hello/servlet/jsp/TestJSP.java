b80497a1f2ee95a200d6f96b787345abff27cc07
foxname91@gmail.com <foxname91@gmail.com>
2025-05-25 23:56:16 +0900

    test git

---
+++ servlet/src/main/java/hello/servlet/jsp/TestJSP.java
@@ -0,0 +1,10 @@
+package hello.servlet.jsp;
+
+public class TestJSP {
+
+    private String name;
+
+    public class findName {
+
+    }
+}
---
+++ servlet/src/test/java/hello/servlet/ServletJspTest.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import org.junit.jupiter.api.Test;
+
+public class ServletJspTest {
+
+    private String name;
+
+    @Test
+    void findName() {
+
+    }
+}
