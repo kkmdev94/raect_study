14e192d85bca73862064b07cd7e58970d3e6c548
foxname91 <foxname91@gmail.com>
2025-06-09 17:58:43 +0900

    message convert

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,7 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.http.converter.HttpMessageConverter;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
