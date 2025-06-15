d750752d830ff821128bcd8e1f5fd76df5ca6e57
foxname91@gmail.com <foxname91@gmail.com>
2025-06-15 18:18:02 +0900

    ModelAttribute

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,15 +29,55 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
-    }
-
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item /*, Model model*/) { // 이로 인해서 모델도 없어도 된다.
+
+        itemRepository.save(item);
+//        model.addAttribute("item", item); // @modelAttribute의 () 안에 있는 item을 통해 모델의 addAttribute 기능을 자동 추가 할 수 있다.
+        // 그래서 item.html에서 th:onclick="|location.href='@{/basic/items/{itemId}/edit(itemId=${item.id})}'|" 이런식으로 item.id에서 그 item을 자동 추가할 수 있다.
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        // V2에서 @ModelAttribute의 ("item")도 지우게 된다면 파라미터의 클래스 명이 Item인데 해당 파라미터로 받는 클래스의 첫글자를 소문자로
+        // 변경해서 모델 애트리뷰트에 담기게 된다. 즉, 지금은 클래스명이 Item이니 item으로 담기고, HelloData라면 helloData로 이렇게 바뀌어서 담기게 된다.
+        // 파라미터로 넘어오는 클래스 명이 해당 호출값과 다르다면 오류가 발생하므로 같은 값만 집어 넣어야 한다.
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+
+        itemRepository.save(item);
+
         return "basic/item";
     }
 
