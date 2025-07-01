475a390050cccb12eeeafaea9b50cf75d28fc0e8
foxname91 <foxname91@gmail.com>
2025-07-01 17:15:07 +0900

    BindingResult

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -7,6 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -42,35 +45,39 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+    @PostMapping("/add") // 주의 : BindingResult 는 항상 ModelAttribute의 객체 바로 뒤에 와야된다.
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
-        //검증 오류 결과를 보관
-        Map<String, String> errors = new HashMap<>();
+//        //검증 오류 결과를 보관
+//        Map<String, String> errors = new HashMap<>();
 
-        //검증 로직
+        //검증 로직(BindingResult를 통한 오류 검증)
         if (!StringUtils.hasText(item.getItemName())) { // 넘어 올때 ItemName이 없다면.
-            errors.put("itemName", "상품 이름은 필수입니다.");
+//            errors.put("itemName", "상품 이름은 필수입니다.");
+            bindingResult.addError(new FieldError("item","itemName","상품 이름은 필수 입니다."));
         }
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            errors.put("price", "가격은 1,000 - 1,000,000원 까지 허용합니다.");
+//            errors.put("price", "가격은 1,000 - 1,000,000원 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item","price","가격은 1,000 - 1,000,000원 까지 허용합니다."));
         }
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
-            errors.put("quantity", "수량은 최대 9,999까지 허용합니다.");
+//            errors.put("quantity", "수량은 최대 9,999까지 허용합니다.");
+            bindingResult.addError(new FieldError("item","quantity","수량은 최대 9,999까지 허용합니다."));
         }
 
         // 특정 필드가 아닌 복합 룰 검증
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
-                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+//                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+                bindingResult.addError(new ObjectError("item","가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
 
         // 검증에 실패하면 다시 입력 폼으로
-        if (!errors.isEmpty()) {
-            log.info("errors = {}", errors);
-            model.addAttribute("errors", errors);
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult = {}", bindingResult);
+//            model.addAttribute("bindingResult", bindingResult); // 생략 가능, 굳이 모델에 안담아도 자동으로 담긴다.
             return "validation/v2/addForm";
         }
 
