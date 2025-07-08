afcf725a3fa3b5376ed01f85f4c3ab4126cba14c
foxname91@gmail.com <foxname91@gmail.com>
2025-07-08 23:10:35 +0900

    poly

---
+++ TypeScript/src/study9.ts
@@ -0,0 +1,28 @@
+interface SStorage<T> {
+    [key:string] : T
+}
+
+class LocalStorage<T> {
+    private storage : SStorage<T> = {}
+    set(key: string, value: T) {
+        this.storage[key] = value;
+    }
+    remove(key: string){
+        delete this.storage[key];
+    }
+    get(key: string):T {
+        return this.storage[key];
+    }
+    clear(){
+        this.storage = {}
+    }
+}
+
+const stringsStorage = new LocalStorage<string>();
+
+stringsStorage.get("key")
+stringsStorage.set("hello","how are you?")
+
+const booleansStorage = new LocalStorage<boolean>();
+booleansStorage.get("xxx")
+booleansStorage.set("hello", false)
