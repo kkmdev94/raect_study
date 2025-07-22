050acd13fb9da60155f297b108b6d8da549b6c73
foxname91 <foxname91@gmail.com>
2025-07-22 15:29:06 +0900

    servlet Filter

---
+++ exception/src/main/java/hello/exception/servlet/ServletExcontroller.java
@@ -0,0 +1,32 @@
+package hello.exception.servlet;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ServletExcontroller {
+
+    /**
+     * Exception이 터진다 -> 서블릿 컨테이너는 500으로 처리, 아니면 컨트롤러를 통해 오류메시지를 직접 담가서 전달.
+     */
+
+    @GetMapping("/error-ex")
+    public void errorEx() {
+        throw new RuntimeException("예외 발생!");
+    }
+
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404, "404 오류!");
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500);
+    }
+}
