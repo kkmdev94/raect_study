222aa4e5d04ecc3f4f1013dc3fde9a4c3ae0424f
foxname91 <foxname91@gmail.com>
2025-07-25 16:13:31 +0900

    converter filed

---
+++ typeconverter/src/test/java/hello/typeconverter/converter/versionServiceTest.java
@@ -0,0 +1,33 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class versionServiceTest {
+
+    @Test
+    void conversionService() {
+        //등록
+        DefaultConversionService conversionService = new DefaultConversionService(); // 컨버전 서비스 인터페이스를 구현한 구현체.
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        // 사용
+//        Integer result = conversionService.convert("10", Integer.class);
+//        System.out.println("result = " + result);
+        assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+        assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+
+        IpPort result = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        Object ipPortString = conversionService.convert(new IpPort("127.0.0.1", 8080), String.class);
+        assertThat(ipPortString).isEqualTo("127.0.0.1:8080");
+    }
+}
