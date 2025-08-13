dd577d0e8fff818932ce6ca68244cdeea7a44b1d
foxname91@gmail.com <foxname91@gmail.com>
2025-08-13 22:54:15 +0900

    dataSource

--- jdbc/src/main/java/hello/jdbc/JdbcApplication.java
+++ jdbc/src/main/java/hello/jdbc/JdbcApplication.java
@@ -2,6 +2,7 @@ package hello.jdbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class JdbcApplication {
