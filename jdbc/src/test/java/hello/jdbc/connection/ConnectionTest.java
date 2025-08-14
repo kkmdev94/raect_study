f88717f919f81447bc5eb976846f17c3e2bd9bab
foxname91@gmail.com <foxname91@gmail.com>
2025-08-14 22:32:35 +0900

    re

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -4,12 +4,10 @@ import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 import static hello.jdbc.connection.ConnectionConst.*;
 
 @Slf4j
