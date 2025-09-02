debf05d0af44952d547192d82aa9a06c8d908910
foxname91 <foxname91@gmail.com>
2025-09-02 15:37:22 +0900

    JDBC Exception

--- jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -4,6 +4,7 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 import hello.jdbc.repository.MemberRepositoryV5_1;
 import hello.jdbc.repository.MemberRepositoryV5_2;
+import hello.jdbc.repository.MemberRepositoryV5_3;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -51,7 +52,8 @@ class MemberServiceV4Test {
         @Bean
         MemberRepository memberRepository() {
 //            return new MemberRepositoryV5_1(dataSource);
-            return new MemberRepositoryV5_2(dataSource);
+//            return new MemberRepositoryV5_2(dataSource);
+            return new MemberRepositoryV5_3(dataSource);
         }
         @Bean
         MemberServiceV5 memberServiceV5() {
