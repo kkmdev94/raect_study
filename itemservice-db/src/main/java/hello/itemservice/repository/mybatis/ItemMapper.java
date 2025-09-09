678ec72977903f5a8fa0c84838345a5df3bfafbd
foxname91 <foxname91@gmail.com>
2025-09-09 11:45:29 +0900

    mapper

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -0,0 +1,23 @@
+package hello.itemservice.repository.mybatis;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface ItemMapper {
+
+    void save(Item item);
+
+    // 파라미터가 한개면 @Param 을 작성 안해도 되지만 2개 이상의 파라미터가 넘어가면 @Param을 붙여넣어야 한다.
+    void update(@Param("id") Long id, @Param("updateParam")ItemUpdateDto updateParam);
+
+    Optional<Item> findById(Long id);
+
+    List<Item> findAll(ItemSearchCond itemSearch);
+}
