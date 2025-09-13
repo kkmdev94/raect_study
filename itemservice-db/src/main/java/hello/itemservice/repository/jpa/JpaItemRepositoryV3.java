16a069df3e83728ebbc62629c5e3c2c14b2d2123
foxname91@gmail.com <foxname91@gmail.com>
2025-09-13 20:43:27 +0900

    start QueryDSL

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
@@ -0,0 +1,76 @@
+package hello.itemservice.repository.jpa;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import hello.itemservice.domain.Item;
+import hello.itemservice.domain.QItem;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Optional;
+
+import static hello.itemservice.domain.QItem.*;
+
+@Repository
+@Transactional
+public class JpaItemRepositoryV3 implements ItemRepository {
+
+    private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public JpaItemRepositoryV3(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em); // 무시해도 큰 문제는 없다.
+    }
+
+    @Override
+    public Item save(Item item) {
+        em.persist(item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = em.find(Item.class, itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        Item item = em.find(Item.class, id);
+        return Optional.ofNullable(item);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+//        QItem item = new QItem("i"); // 내부적으로 QItem을 가지고 있기에 아래처럼 QItem.item으로 해주는게 더 좋다. 스태틱 메서드라 스태틱으로 지정도 가능.
+        // QueryDSL의 강점. / 동적 쿼리를 자바코드로 만들어서 지정.
+        BooleanBuilder builder = new BooleanBuilder();
+        if (StringUtils.hasText(itemName)){
+            builder.and(item.itemName.like("%" + itemName + "%"));
+        }
+        if (maxPrice != null) {
+            builder.and(item.price.loe(maxPrice));
+        }
+
+        List<Item> result = query
+                .select(item)
+                .from(item)
+                .where(builder)
+                .fetch();
+
+        return result;
+    }
+}
