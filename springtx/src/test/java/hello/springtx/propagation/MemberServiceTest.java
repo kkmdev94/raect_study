4040de0c717dc30a697d9e59daf69f6f9a8ab59d
foxname91@gmail.com <foxname91@gmail.com>
2025-09-21 19:00:27 +0900

    Requred

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -109,6 +110,32 @@ class MemberServiceTest {
 
         //then : 모든 데이터가 롤백된다.
         assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService    @Transactional:ON // 외부
+     * memberRepository @Transactional:ON //  내부
+     * logRepository    @Transactional:ON Exception // 내부
+     *
+     * 내부에서 예외가 발생하면 트랜잭션 AOP가 해당 예외를 받는데, 이때 이 내부 예외는 신규 트랜잭션이 아니므로 물리 트랜잭션을 롤백하지 않고
+     * 트랜잭션 동기화 매니저에 rollbackOnly를 표시하고 예외를 밖으로 던진다.
+     *
+     * 그 예외는 memberService에 던져지고 우리가 만든 try catch 로직으로 인해 정상 흐름으로 리턴, 이 정상 흐름 리턴으로 MemberService의 AOP를 호출
+     * 이때 MemberService의 트랜잭션은 외부의 트랜잭션으로 신규 트랜잭션이므로 물리 트랜잭션을 호출할 수 있고 물리 트랜잭션이 commit을 하려하지만
+     * commit을 하려는 과정에서 rollbackOnly를 확인, 따라서 물리 트랜잭션은 전체를 다 rollback 시키는 것이다. (자주 일어나는 예외 현상이므로 주의깊게 한번더 학습하자)
+     */
+    @Test
+    void recoverException_fail() {
+        //given
+        String username = "로그예외_recoverException_fail";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV2(username))
+                .isInstanceOf(UnexpectedRollbackException.class);
+
+        //then : 모든 데이터가 롤백된다.
+        assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isEmpty());
     }
 }
