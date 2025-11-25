85aa57d5c7f58b7bdc877368d1bfaba74183c798
foxname91@gmail.com <foxname91@gmail.com>
2025-11-25 22:20:49 +0900

    SEL

--- jpashop/src/main/java/jpabook/jpashop/InitDb.java
+++ jpashop/src/main/java/jpabook/jpashop/InitDb.java
@@ -4,7 +4,6 @@ import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.item.Book;
-import jpabook.jpashop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,57 +36,60 @@ public class InitDb {
         private final EntityManager em;
         
         public void dbinit1() {
-            Member member = new Member();
-            member.setName("userA");
-            member.setAddress(new Address("seoul","1","1111"));
+            Member member = createMember("userA","seoul","1","1111");
             em.persist(member);
 
-            Book book1 = new Book();
-            book1.setName("JPA1 BOOK");
-            book1.setPrice(10000);
-            book1.setStockQuantity(100);
+            Book book1 = createBook("JPA2 BOOK1" ,10000, 100);
             em.persist(book1);
 
-            Book book2 = new Book();
-            book2.setName("JPA2 BOOK");
-            book2.setPrice(20000);
-            book2.setStockQuantity(100);
+            Book book2 = createBook("JPA2 BOOK1" ,10000, 100);
             em.persist(book2);
 
             OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
             OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
 
-            Delivery delivery = new Delivery();
-            delivery.setAddress(member.getAddress());
+            Delivery delivery = createDelivery(member);
             Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
             em.persist(order);
         }
 
         public void dbinit2() {
-            Member member = new Member();
-            member.setName("userA");
-            member.setAddress(new Address("busan","2","2222"));
+            Member member =  createMember("userB","busan","2","2222");
             em.persist(member);
 
-            Book book1 = new Book();
-            book1.setName("JPA1 BOOK");
-            book1.setPrice(10000);
-            book1.setStockQuantity(100);
+            Book book1 = createBook("SPRING BOOK1", 20000, 100);
             em.persist(book1);
 
-            Book book2 = new Book();
-            book2.setName("JPA2 BOOK");
-            book2.setPrice(20000);
-            book2.setStockQuantity(100);
+            Book book2 = createBook("SPRING BOOK2",20000, 100);
             em.persist(book2);
 
-            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
-            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+            OrderItem orderItem3 = OrderItem.createOrderItem(book1, 30000, 4);
+            OrderItem orderItem4 = OrderItem.createOrderItem(book2, 40000, 3);
 
+            Delivery delivery = createDelivery(member);
+            Order order = Order.createOrder(member, delivery, orderItem3, orderItem4);
+            em.persist(order);
+        }
+
+        private static Delivery createDelivery(Member member) {
             Delivery delivery = new Delivery();
             delivery.setAddress(member.getAddress());
-            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
-            em.persist(order);
+            return delivery;
+        }
+
+        private Book createBook(String s, int i, int stockQuantity) {
+            Book book1 = new Book();
+            book1.setName(s);
+            book1.setPrice(i);
+            book1.setStockQuantity(stockQuantity);
+            return book1;
+        }
+
+        private Member createMember(String name, String city, String street, String zipcode) {
+            Member member = new Member();
+            member.setName(name);
+            member.setAddress(new Address(city, street, zipcode));
+            return member;
         }
     }
 }
