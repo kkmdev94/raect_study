ee966fb0e9f3ca15a3bd29838c19c79807c8baf7
foxname91@gmail.com <foxname91@gmail.com>
2025-12-15 22:28:18 +0900

    DTO

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,7 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.jaxb.SpringDataJaxb;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,19 +43,49 @@ public class OrderApiController {
         return collect;
     }
 
+    /**
+     * DTO로 반환해라고 했을때 엔티티가 노출되면 안된다.
+     * 아래의 로직처럼 랩핑도 안된다. POSTMAN으로 돌려보면 OrderItems의 스펙이 즉, 데이터가 외부에 전부 노출된다는 것이다.
+     * 엔티티에 대한 의존을 완전히 끊어야 한다.
+     */
+    @Getter
     static class OrderDto {
         private Long orderId;
         private String name;
         private LocalDateTime orderDate;
         private OrderStatus orderStatus;
         private Address address;
+//        private List<OrderItem> orderItems;
+        private List<OrderItemDto> orderItems;
 
-        public OrderDto(Order o) {
-            orderId = o.getId();
-            name = o.getMember().getName();
-            orderDate = o.getOrderDate();
-            orderStatus = o.getStatus();
-            address = o.getDelivery().getAddress();
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+//            order.getOrderItems().stream().forEach(o -> o.getItem().getName()); -> OrderItem 조차도 다 DTO로 바꾸어서 넘겨야 한다.
+//            orderItems = order.getOrderItems();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    /**
+     * 이렇게 한번더 DTO로 랩핑해서 나가기 떄문에 깔끔하게 엔티티 노출 없이 정보를 넘길 수 있다.
+     */
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName; // 클라이언트의 요구가 3개라고 가정했을때.
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
         }
     }
 }
